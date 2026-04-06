d0b93fd83db45133a949005159b43e04b2837ef5
remnantserenityy <remnantserenityy@tuta.com>
2026-04-06 19:23:38 +0200

    got a black window running

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+        sf::RenderWindow window(sf::VideoMode({800, 600}), "platform-sandbox");
+
+        while (window.isOpen()) {
+                while (const std::optional event = window.pollEvent()) {
+                        if (event->is<sf::Event::Closed>()) window.close();
+                }
+
+                window.clear(sf::Color::Black);
+                window.display();
+        }
+
+        return 0;
+}
