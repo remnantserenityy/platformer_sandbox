84b2499c54fa3ffd620ca6cbee80740401b52d95
remnantserenityy <remnantserenityy@tuta.com>
2026-04-06 19:18:10 +0200

    basic window running

---
+++ build/CMakeFiles/platform_sandbox.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/platform_sandbox.dir/link.d"
+  "CMakeFiles/platform_sandbox.dir/src/main.cpp.o"
+  "CMakeFiles/platform_sandbox.dir/src/main.cpp.o.d"
+  "platform_sandbox"
+  "platform_sandbox.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/platform_sandbox.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
